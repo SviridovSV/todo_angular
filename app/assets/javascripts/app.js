50ff69ec7e07bdd46cd5d5460c43c121ea13c6c4
Sergey <sviridov.sv82@gmail.com>
2017-12-04 23:34:30 +0200

    setup angular, create user model

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,35 @@
+angular.module('todoList', ['ui.router', 'templates', 'Devise'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'auth/login.html',
+      controller: 'MainCtrl',
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    })
+    .state('register', {
+      url: '/signup',
+      templateUrl: 'auth/register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
