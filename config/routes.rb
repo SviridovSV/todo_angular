d0192c00e04cf48595e9c5f45f73216841fbe146
Sergey <sviridov.sv82@gmail.com>
2017-12-12 10:24:51 +0200

    add facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
   root to: 'application#angular'
 
   resources :projects, only: [:index, :create, :update, :destroy]
